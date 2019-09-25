5d5b618f0b34bb2c0eb8058d57406c3ba78644d9
t00048641 <travis.m.slattery@students.ittralee.ie>
2019-09-25 10:59:17 +0100

    added new player class

--- src/blackJack/GamePlayer.java
+++ src/blackJack/GamePlayer.java
@@ -2,7 +2,7 @@ package blackJack;
 
 public class GamePlayer {
     public static void main(String[] args) {
-        Deck myDeck = new Deck(1, true);
-        myDeck.printDeck(24);
+
+        
     }
 }
